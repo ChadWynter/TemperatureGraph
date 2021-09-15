4956fa63a8ef7bd7716956624d1d42826af2d6a1
Chad Wynter <chad-wynter@uiowa.edu>
2021-09-15 10:52:28 -0500

    no changes

--- TemperatureGraph/TemperatureGraph.py
+++ TemperatureGraph/TemperatureGraph.py
@@ -2,9 +2,9 @@ import matplotlib.pyplot as plt
 import serial
 import numpy as np
 
-#ser = serial.Serial("COM3", 9600)
-#while True:
-#        cc=str(ser.readLine())
+ser = serial.Serial("COM3", 9600)
+while True:
+        cc=str(ser.readLine())
 
 
 x = np.linspace(0, 10*np.pi, 100)
