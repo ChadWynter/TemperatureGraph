df8937d391742b0b9f55ab5dcf5e7c3c719ea2f6
charliewolfe <cjwolfe@uiowa.edu>
2021-09-19 16:16:46 -0500

    file content

---
+++ TemperatureGraph/firebase_db.py
@@ -0,0 +1,22 @@
+import pyrebase
+
+config = {
+  "apiKey": "apiKey",
+  "authDomain": "projectId.firebaseapp.com",
+  "databaseURL": "https://ece-4880-lab1-default-rtdb.firebaseio.com/",
+  "storageBucket": "projectId.appspot.com"
+}
+
+def get_temp(db):
+    return db.child("Temperature").get().val()
+
+def set_temp(db, temp):
+    db.child("Temperature").push(temp)
+
+if __name__ == "__main__":
+    firebase = pyrebase.initialize_app(config)
+    db = firebase.database()
+    current_temp = get_temp(db)
+    print(current_temp)
+    # set_temp(db, 10)
+    
