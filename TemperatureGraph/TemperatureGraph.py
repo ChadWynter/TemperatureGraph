a9cce493c6046aebb2152c998d0391f8e9078fbb
Chad Wynter <chad-wynter@uiowa.edu>
2021-09-19 17:19:45 -0500

    need x and y data labels

--- TemperatureGraph/TemperatureGraph.py
+++ TemperatureGraph/TemperatureGraph.py
@@ -1,25 +1,43 @@
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Button
-import serial
 import numpy as np
+import pyrebase
+  
+import pyrebase
 
-#ser = serial.Serial("COM3", 9600)
-#while True:
-#        cc=str(ser.readLine())
+config = {
+  "apiKey": "apiKey",
+  "authDomain": "projectId.firebaseapp.com",
+  "databaseURL": "https://ece-4880-lab1-default-rtdb.firebaseio.com/",
+  "storageBucket": "projectId.appspot.com"
+}
 
+def get_temp(db):
+    return db.child("Temperature").get().val()
 
+def set_temp(db, temp):
+    db.child("Temperature").push(temp)
 
-#code for interactive graph
+if __name__ == "__main__":
+    firebase = pyrebase.initialize_app(config)
+    db = firebase.database()
+    current_temp = get_temp(db)
+    print(current_temp)
+    # set_temp(db, 10)
+    
 x = np.linspace(0, 10*np.pi, 100)
 y = np.sin(x)
 
 plt.ion()
 fig = plt.figure()
 ax = fig.add_subplot(111)
+#ax = fig.add_axes([0,0,1,1])
+line1, = ax.plot(x, y, 'b-')
 ax.set_title("Temperature Data")
 ax.set_xlabel("Time")
 ax.set_ylabel("Temperature in C")
-line1, = ax.plot(x, y, 'b-')
+#ax.set_ylim(-100,100)
+#ax.set_ylim(-100,100)
 
 #need to resize button
 def on_click(event):
