1e6451c3bf1b00506ef53bc78f99499395ce6b59
charliewolfe <cjwolfe@uiowa.edu>
2021-09-26 17:22:47 -0500

    added code to send sms

---
+++ TemperatureGraph/sms.py
@@ -0,0 +1,21 @@
+import smtplib
+
+'''
+verizon: @vtext.com
+att: @mms.att.net
+tmobile: @tmomail.net
+'''
+
+email = "ece.4880.lab@gmail.com"
+password = "KK5PF6qc9GKWhT"
+
+server = smtplib.SMTP("smtp.gmail.com", 587)
+server.starttls()
+server.login(email, password)
+
+def send_sms(number, message):
+    server.sendmail(email, number, message)
+
+if __name__ == "__main__":
+    send_sms("6125997855@vtext.com", "test123")
+    
