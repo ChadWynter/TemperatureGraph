9462f748e7f131afde7db666ca164d222ef812f4
Chad Wynter <chad-wynter@uiowa.edu>
2021-09-28 17:12:42 -0500

    button acting stupid

--- TemperatureGraph/TemperatureGraph.py
+++ TemperatureGraph/TemperatureGraph.py
@@ -1,55 +1,138 @@
 import matplotlib.pyplot as plt
-from matplotlib.widgets import Button
+from matplotlib.widgets import Button, TextBox
 import numpy as np
-# import pyrebase
+import pyrebase
+from time import sleep
+from collections import OrderedDict
+import time
 
-# config = {
-#   "apiKey": "apiKey",
-#   "authDomain": "projectId.firebaseapp.com",
-#   "databaseURL": "https://ece-4880-lab1-default-rtdb.firebaseio.com/",
-#   "storageBucket": "projectId.appspot.com"
-# }
 
-# def get_temp(db):
-#     return db.child("Temperature").get().val()
+config = {
+  "apiKey": "apiKey",
+  "authDomain": "projectId.firebaseapp.com",
+  "databaseURL": "https://ece-4880-lab1-default-rtdb.firebaseio.com/",
+  "storageBucket": "projectId.appspot.com"
+}
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+def send_button_press(db, change):
+    db.child("ReadTemperatureButton").set(change)
 
-# def set_temp(db, temp):
-#     db.child("Temperature").push(temp)
 
-# if __name__ == "__main__":
-#     firebase = pyrebase.initialize_app(config)
-#     db = firebase.database()
-#     current_temp = get_temp(db)
-#     print(current_temp)
-#     # set_temp(db, 10)
-    
-x = np.linspace(0, 10*np.pi, 100)
-y = np.sin(x)
+def get_stored_temperatures(db):
+    temp_data = db.child("Temperature").get().val()
+    time_data = db.child("Time").get().val()
+
+    final_temp_data = []
+    final_time_data = []
+    for element in temp_data:
+        final_temp_data.append(temp_data[element])
+
+    for element in time_data:
+        final_time_data.append(time_data[element])
+
+    return np.array(final_temp_data), np.array(final_time_data)
+
+    # set_temp(db, 10)
+
+#getting the stored temperatures
+a, Time = get_stored_temperatures(db)
+send_button_press(db,0)
+
+#test to convert array to float
+e = np.array(['nil', 'nil', 3, 4, 5])
+f = np.array([])
+for x in e:
+    if x == 'nil':
+        f = np.append(f, None)
+    else:
+        f = np.append(f,float(x))
+#print(f)
+
+#should convert array to float
+temp = np.array([])
+for i in a:
+    if i == 'nil':
+        temp = np.append(temp, None)
+    else:
+        temp = np.append(temp,float(i))
+if temp.size == 301:
+    temp = np.delete(temp, 0)
+temp = np.flip(temp)
+#print(temp)
+
+
 
 plt.ion()
-fig = plt.figure()
-ax = fig.add_subplot(111)
+fig, ax = plt.subplots(sharex = True)
+x = np.arange(1,301)
+line1, = ax.plot(x,temp, 'b-')
+plt.gcf().set_size_inches(9.5, 10)
+ 
 
-#ax = fig.add_axes([0,0,1,1])
-line1, = ax.plot(x, y, 'b-')
+ax.invert_xaxis()
+ax.set_ylim([10,50])
 ax.set_title("Temperature Data")
-ax.set_xlabel("Time")
+ax.set_xlabel("Time (seconds ago)")
 ax.set_ylabel("Temperature in C")
-plt.xticks([1,2,3,4,5])
-#ax.set_ylim(-100,100)
-#ax.set_ylim(-100,100)
+ 
+#button code starts here (changes only stay when anywhere but the button is pressed)
 
-#need to resize button
 def on_click(event):
     if ("Temperature in C" in ax.get_ylabel()):
         ax.set_ylabel("Temperature in F")
+        ax.set_ylim([50,122])
     else:
         ax.set_ylabel("Temperature in C")
+        ax.set_ylim([10,50])
+    #fig.canvas.draw()
+    #fig.canvas.flush_events()
+    
+#defining real time temperature textbox
+graphBox = fig.add_axes([0.3, 0.92, 0.5, 0.075])
+txtBox = TextBox(graphBox, "Temperature: ")
+txtBox.set_val(str(temp[temp.size - 1]) + " Degrees C")
+
+
+#defining button
+axes = plt.axes([0.81, 0.000001, 0.1, 0.075])
+temp_labelbutton = Button(axes, "change units", color = "blue")
+temp_labelbutton.on_clicked(on_click)
+props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
 plt.connect('button_press_event', on_click)
-bcut = Button(ax, 'change measurement', color = 'red', hovercolor = 'green')
+while True:
+    last_time = Time[Time.size - 1]
+    
 
-
-for phase in np.linspace(0, 10*np.pi, 500):
-    line1.set_ydata(np.sin(0.5 * x + phase))
     fig.canvas.draw()
     fig.canvas.flush_events()
+    sleep(1)
+    a, Time = get_stored_temperatures(db)
+    if Time[Time.size-1] == last_time:
+        #display error message
+        print("error message")
+
+    #should convert array to float
+    temp = np.array([])
+
+    for i in a:
+        if i == 'nil':
+            temp = np.append(temp, None)
+        else:
+            temp = np.append(temp,float(i))
+
+    if temp.size == 301:
+        temp = np.delete(temp, 0)
+
+    temp = np.flip(temp)
+    line1.remove()
+    line1, = ax.plot(x,temp, 'b-')
+    
+
+    #real time temperature
+    if ("Temperature in C" in ax.get_ylabel()):
+        txtBox.set_val(str(temp[temp.size - 1]) + " Degrees C")
+
+    else:
+        txtBox.set_val(str(temp[temp.size - 1]) + " Degrees F")
+
